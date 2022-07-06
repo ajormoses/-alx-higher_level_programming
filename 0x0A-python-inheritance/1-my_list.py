c38789bac6d7a61d5805a9189aee4d7dce189df1
ajormoses <ajormoses@gmail.com>
2022-07-06 22:39:47 +0100

    commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
+class MyList(list):
+    """a subclass of list"""
+    def print_sorted(self):
+        """prints the sorted list (ascending sort)"""
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""contains a is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    check if object is an instance of,
+
+    Args:
+       obj: obj argument
+       a_class: a_class to validate
+
+    Return:
+       True or false
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
